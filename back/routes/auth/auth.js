231d5039a221a798a9c11428ff6d0df6a8a6c8f1
Corinne Russo <corinne.russo@gmail.com>
2019-12-30 10:31:01 +0100

    quete 040

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const connection = require("../helpers/db.js");
 
-router.post("/signup", (req, res, next) => {
-  res.send("I am in POST signup");
+router.post("/signup", function(req, res, next) {
+  const formData = req.body;
+
+  connection.query("Insert INTO users SET ?", formData, function(
+    error,
+    results,
+    fields
+  ) {
+    res.send(`ok for ${formData.name}`);
+    if (error) {
+      res.status(500).end("Erreur");
+    }
+
+    res.end();
+  });
 });
 
 module.exports = router;
